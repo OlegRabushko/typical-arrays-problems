cb61a7f0e8d9621e3d1f404d53624dadb71e2527
Oleg <oleg.rabyshko@mail.ru>
2021-02-10 13:56:35 +0300

    feat: add pased task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    if (!array || array.length < 1) {
+        return 0;
+    }
+    return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length < 1) {
+        return 0;
+    }
+    return Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length < 1) {
+        return 0;
+    }
+    let b = array.reduce((acc, el) => {
+        return acc + el;
+    }, 0);
+    return (b / array.length).toFixed(2);
+};
